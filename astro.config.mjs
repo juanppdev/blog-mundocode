3a20d464cd1cdd945b4eeaf48508071c9294b0fa
Juan Pablo Patino Lopez <fomundi.fr@gmail.com>
2024-02-26 17:38:39 +0100

    add feedback

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,47 +5,45 @@ import compressor from "astro-compressor";
 import sitemap from '@astrojs/sitemap';
 import robotsTxt from 'astro-robots-txt';
 import { VitePWA } from 'vite-plugin-pwa';
-
 import { manifest } from './src/utils/manifest';
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog-mundocode.vercel.app/',
   image: {
-    remotePatterns: [{ protocol: 'https' }],
+    remotePatterns: [{
+      protocol: 'https'
+    }]
   },
   markdown: {
     drafts: true,
     shikiConfig: {
       theme: 'material-theme-palenight',
-      wrap: true,
+      wrap: true
+    }
+  },
+  integrations: [mdx({
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      theme: 'material-theme-palenight',
+      wrap: true
     },
-  },
-  integrations: [
-    mdx({
-      syntaxHighlight: 'shiki',
-      shikiConfig: {
-        theme: 'material-theme-palenight',
-        wrap: true,
-      },
-      drafts: true,
-    }),
-    compressor({ gzip: true, brotli: true }),
-    sitemap(),
-    tailwind(),
-    robotsTxt(),
-  ],
+    drafts: true
+  }), compressor({
+    gzip: true,
+    brotli: true
+  }), sitemap(), tailwind(), robotsTxt(), react()],
   vite: {
-    plugins: [
-      VitePWA({
-        registerType: 'autoUpdate',
-        manifest,
-        workbox: {
-          globDirectory: 'dist',
-          globPatterns: ['**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}'],
-          navigateFallback: null,
-        },
-      }),
-    ],
-  },
+    plugins: [VitePWA({
+      registerType: 'autoUpdate',
+      manifest,
+      workbox: {
+        globDirectory: 'dist',
+        globPatterns: ['**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}'],
+        navigateFallback: null
+      }
+    })]
+  }
 });
