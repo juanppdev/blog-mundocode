ce9a09ece49ea689dc5c34afd4066981baa2f6bd
Juan Pablo Patino Lopez <fomundi.fr@gmail.com>
2024-02-27 15:40:37 +0100

    nueva subida

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,52 +5,47 @@ import compressor from "astro-compressor";
 import sitemap from '@astrojs/sitemap';
 import robotsTxt from 'astro-robots-txt';
 import { VitePWA } from 'vite-plugin-pwa';
-import { manifest } from './src/utils/manifest';
 
-import react from "@astrojs/react";
+import { manifest } from './src/utils/manifest';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://blog-mundocode.vercel.app/',
+  site: 'http://localhost:4322/',
   image: {
-    remotePatterns: [{
-      protocol: 'https'
-    }]
+    remotePatterns: [{ protocol: 'https' }],
   },
   markdown: {
     drafts: true,
     shikiConfig: {
       theme: 'material-theme-palenight',
-      wrap: true
-    }
-  },
-  integrations: [mdx({
-    syntaxHighlight: 'shiki',
-    shikiConfig: {
-      theme: 'material-theme-palenight',
-      wrap: true
+      wrap: true,
     },
-    drafts: true
-  }), compressor({
-    gzip: true,
-    brotli: true
-  }), sitemap(), tailwind(), robotsTxt(), react()],
-  vite: {
-    plugins: [VitePWA({
-      registerType: 'autoUpdate',
-      manifest,
-      build: {
-        rollupOptions: {
-          external: [
-            '@config/config.json',
-          ],
-        },
+  },
+  integrations: [
+    mdx({
+      syntaxHighlight: 'shiki',
+      shikiConfig: {
+        theme: 'material-theme-palenight',
+        wrap: true,
       },
-      workbox: {
-        globDirectory: 'dist',
-        globPatterns: ['**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}'],
-        navigateFallback: null
-      }
-    })]
-  }
+      drafts: true,
+    }),
+    compressor({ gzip: true, brotli: true }),
+    sitemap(),
+    tailwind(),
+    robotsTxt(),
+  ],
+  vite: {
+    plugins: [
+      VitePWA({
+        registerType: 'autoUpdate',
+        manifest,
+        workbox: {
+          globDirectory: 'dist',
+          globPatterns: ['**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}'],
+          navigateFallback: null,
+        },
+      }),
+    ],
+  },
 });
